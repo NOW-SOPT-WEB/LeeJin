0aa2e5f65bcf89cdc043194521d349737f1a7968
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-04-19 02:23:15 +0900

    feat: 장바구니 페이지 레이아웃

--- ShoppingList/script.js
+++ ShoppingList/script.js
@@ -1,33 +1,31 @@
 import { SHOPPING_LIST } from './data.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('content');
-    const categoryList = document.getElementById('categoryList');
+const container = document.getElementById('content');
+const categoryList = document.getElementById('categoryList');
 
-    categoryList.addEventListener('click', (e) => {
-        const category = e.target.getAttribute('category');
-        if (!category) return;
+categoryList.addEventListener('click', (e) => {
+    const category = e.target.getAttribute('category');
+    if (!category) return;
 
-        filterItems(category);
-    });
+    filterItems(category);
+});
 
-    function filterItems(category) {
-        const itemsToDisplay = category === '전체' ?
-            Object.values(SHOPPING_LIST).flat():
-            SHOPPING_LIST[category];
-        
-        const categoryHtml = itemsToDisplay.map(item => `
-            <article class="card">
-                <img src="${item.src}" alt="${item.name}">
-                <div class="card-info">
-                    <div class="heart-icon">♥</div>
-                    <h3>${item.name}</h3>
-                    <div class="price">${item.price}RP</div>
-                </div>
-            </article>
-        `).join(``);
-        container.innerHTML = `<section class="card-container">${categoryHtml}</secion>`;
-    }
+function filterItems(category) {
+    const itemsToDisplay = category === '전체' ?
+        Object.values(SHOPPING_LIST).flat():
+        SHOPPING_LIST[category];
+    
+    const categoryHtml = itemsToDisplay.map(item => `
+        <article class="card">
+            <img src="${item.src}" alt="${item.name}">
+            <div class="card-info">
+                <div class="heart-icon">♥</div>
+                <h3>${item.name}</h3>
+                <div class="price">${item.price}RP</div>
+            </div>
+        </article>
+    `).join(``);
+    container.innerHTML = `<section class="card-container">${categoryHtml}</secion>`;
+}
 
-    filterItems('전체');
-})
+filterItems('전체');
