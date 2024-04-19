64c6997b4e99939eabe55547bd16e9c9d31e26e6
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-04-19 23:19:24 +0900

    fix: 이미지 한글 오류 수정 -> 영문화

--- ShoppingList/data.js
+++ ShoppingList/data.js
@@ -1,31 +1,31 @@
 export const SHOPPING_LIST = {
     "미스포츈" : [
-        {src:"./assets/section1/도자기미포.jpeg", name:"도자기 미스 포츈", price:1350, category: "미스포츈"},
-        {src:"./assets/section1/전투토끼미포.jpeg", name:"전투토끼 미스 포츈", price:1820, category: "미스포츈"},
-        {src:"./assets/section1/카우걸미포.jpeg", name:"카우걸 미스 포츈", price:750, category: "미스포츈"},
-        {src:"./assets/section1/워털루미포.jpeg", name:"워털루 미스 포츈", price:520, category: "미스포츈"},
-        {src:"./assets/section1/몰락미포.jpeg", name:"몰락한 미스 포츈", price:1350, category: "미스포츈"},
-        {src:"./assets/section1/범죄도시미포.jpeg", name:"범죄도시 미스 포츈", price:975, category: "미스포츈"},
-        {src:"./assets/section1/파자마미포.jpeg", name:"파자마 수호자 미스 포츈", price:1350, category: "미스포츈"},
+        {src:"./assets/section1/mipo1.jpeg", name:"도자기 미스 포츈", price:1350, category: "미스포츈"},
+        {src:"./assets/section1/mipo2.jpeg", name:"전투토끼 미스 포츈", price:1820, category: "미스포츈"},
+        {src:"./assets/section1/mipo3.jpeg", name:"카우걸 미스 포츈", price:750, category: "미스포츈"},
+        {src:"./assets/section1/mipo4.jpeg", name:"워털루 미스 포츈", price:520, category: "미스포츈"},
+        {src:"./assets/section1/mipo5.jpeg", name:"몰락한 미스 포츈", price:1350, category: "미스포츈"},
+        {src:"./assets/section1/mipo6.jpeg", name:"범죄도시 미스 포츈", price:975, category: "미스포츈"},
+        {src:"./assets/section1/mipo7.jpeg", name:"파자마 수호자 미스 포츈", price:1350, category: "미스포츈"},
     ],
     "쓰레쉬" : [
-        {src:"./assets/section2/겨축쓰.jpeg", name:"겨울의 축복 쓰레쉬", price:1350, category: "쓰레쉬"},
-        {src:"./assets/section2/달빛쓰.jpeg", name:"달빛 황제 쓰레쉬", price:1350, category: "쓰레쉬"},
-        {src:"./assets/section2/암흑쓰.jpeg", name:"암흑의 별 쓰레쉬", price:1820, category: "쓰레쉬"},
-        {src:"./assets/section2/핏쓰.jpeg", name:"핏빛달 쓰레쉬", price:975, category: "쓰레쉬"},
-        {src:"./assets/section2/영꽃쓰.jpeg", name:"영혼의 꽃 쓰레쉬", price:1820, category: "쓰레쉬"},
-        {src:"./assets/section2/해쓰.jpeg", name:"해방된 쓰레쉬", price:750, category: "쓰레쉬"},
-        {src:"./assets/section2/펄쓰.jpeg", name:"펄스 건 쓰레쉬", price:1350, category: "쓰레쉬"},
+        {src:"./assets/section2/trash1.jpeg", name:"겨울의 축복 쓰레쉬", price:1350, category: "쓰레쉬"},
+        {src:"./assets/section2/trash2.jpeg", name:"달빛 황제 쓰레쉬", price:1350, category: "쓰레쉬"},
+        {src:"./assets/section2/trash3.jpeg", name:"암흑의 별 쓰레쉬", price:1820, category: "쓰레쉬"},
+        {src:"./assets/section2/trash4.jpeg", name:"핏빛달 쓰레쉬", price:975, category: "쓰레쉬"},
+        {src:"./assets/section2/trash5.jpeg", name:"영혼의 꽃 쓰레쉬", price:1820, category: "쓰레쉬"},
+        {src:"./assets/section2/trash6.jpeg", name:"해방된 쓰레쉬", price:750, category: "쓰레쉬"},
+        {src:"./assets/section2/trash7.jpeg", name:"펄스 건 쓰레쉬", price:1350, category: "쓰레쉬"},
     ],
     "사미라" : [
-        {src:"./assets/section3/소파사.jpeg", name:"소울 파이터 사미라", price:3250, category: "사미라"},
-        {src:"./assets/section3/우그사.jpeg", name:"우주 그루브 사미라", price:1350, category: "사미라"},
-        {src:"./assets/section3/초특사.jpeg", name:"초능력특공대 사미라", price:1350, category: "사미라"},
-        {src:"./assets/section3/하눈사.jpeg", name:"하이 눈 사미라", price:1350, category: "사미라"},
+        {src:"./assets/section3/samira1.jpeg", name:"소울 파이터 사미라", price:3250, category: "사미라"},
+        {src:"./assets/section3/samira2.jpeg", name:"우주 그루브 사미라", price:1350, category: "사미라"},
+        {src:"./assets/section3/samira3.jpeg", name:"초능력특공대 사미라", price:1350, category: "사미라"},
+        {src:"./assets/section3/samira4.jpeg", name:"하이 눈 사미라", price:1350, category: "사미라"},
     ],
     "유미" : [
-        {src:"./assets/section4/꿀잼냥.jpeg", name:"꿀잼 유미", price:1350, category: "유미"},
-        {src:"./assets/section4/시바냥.jpeg", name:"시바견 유미", price:1350, category: "유미"},
-        {src:"./assets/section4/전사냥.jpeg", name:"전투사관학교 교장 유미", price:1350, category: "유미"}
+        {src:"./assets/section4/cat1.jpeg", name:"꿀잼 유미", price:1350, category: "유미"},
+        {src:"./assets/section4/cat2.jpeg", name:"시바견 유미", price:1350, category: "유미"},
+        {src:"./assets/section4/cat3.jpeg", name:"전투사관학교 교장 유미", price:1350, category: "유미"}
     ]
 };
