40dcdc442b6d9305c7513de395a538dd2cc8dcd5
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-04-19 20:27:24 +0900

    feat: localStorage에서 가져온 데이터로 테이블 삽입

---
+++ ShoppingList/cart/script.js
@@ -0,0 +1,47 @@
+const cartData = JSON.parse(localStorage.getItem('cart')) || [];
+console.log({cartData})
+const tableBody = document.getElementById('cartTable').querySelector('tbody');
+cartData.forEach(item => {
+    const row = tableBody.insertRow();
+
+    //선택 셀
+    const selectCell = row.insertCell(0);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.style.cursor = 'pointer';
+    selectCell.appendChild(checkbox);
+
+    //상품 사진 셀
+    const imageCell = row.insertCell(1);
+    const image = document.createElement('img');
+    image.src = '.' + item.src;
+    image.alt = item.name;
+    image.style.width = '2rem';
+    imageCell.appendChild(image);
+
+    //상품명 셀
+    const nameCell = row.insertCell(2);
+    nameCell.textContent = item.name;
+
+    //상품금액 셀
+    const priceCell = row.insertCell(3);
+    priceCell.textContent = item.price + 'RP';
+
+    //카테고리 셀
+    const categoryCell = row.insertCell(4);
+    categoryCell.textContent = item.category;
+
+    //삭제버튼 셀
+    const deleteCell = row.insertCell(5);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '삭제';
+    deleteButton.onClick = () => {
+        tableBody.removeChild(row);
+        const index = cartData.indexOf(item);
+        if (index > -1) {
+            cartData.splice(index, 1);
+            localStorage.setItem('cart', JSON.stringify(cartData));
+        }
+    };
+    deleteCell.appendChild(deleteButton);
+});
