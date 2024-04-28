192220a415bcc19a4133aa9069d4faa6f7ef2e37
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-04-29 06:44:30 +0900

    style: button 디자인 변경

--- CardGame/src/components/Header/styles.js
+++ CardGame/src/components/Header/styles.js
@@ -15,17 +15,39 @@ export const Title = styled.div`
     font-size: 2.5rem;
 `;
 
-export const resetBtn = styled.button`
+export const resetBtn = styled.div`
     display: flex;
+    align-items: center;
     position: absolute;
     right: 1rem;
 
-    align-items: center;
     height: 2.5rem;
-    padding: 1rem;
+    padding: 1.5rem 2rem;
 
-    font-family: var(--font-평창평화);
     cursor: pointer;
+    border-radius: 6px;
+
+    color: white;
+
+    transition: top .01s linear;
+    text-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
+
+    background-color: #F1C3CC;
+    box-shadow: 0 0 0 1px #D69EAA inset,
+            0 0 0 2px rgba(255,255,255,0.15) inset,
+            0 8px 0 0 #D69EAA,
+            0 8px 0 1px rgba(0,0,0,0.4),
+            0 8px 8px 1px rgba(0,0,0,0.5);
+
+    &:hover {
+        background-color: #E6AFBB;
+    }
+
+    &:active {
+        box-shadow: 0 0 0 1px #D69EAA inset,
+                0 0 0 2px rgba(255,255,255,0.15) inset,
+                0 0 0 1px rgba(0,0,0,0.4);
+    }
 `;
 
 export const Subtitle = styled.div`
--- CardGame/src/components/LevelBtn/styles.js
+++ CardGame/src/components/LevelBtn/styles.js
@@ -19,19 +19,19 @@ export const Btn = styled.div`
     font-size: 1rem;
     border-radius: 6px;
     transition: top .01s linear;
-    text-shadow: 0 1px 0 rgba(0,0,0,0.15);
+    text-shadow: 0 2px 0 rgba(0, 0, 0, 0.15);
 
-    background-color: ${props => props.clicked ? "#F0D264" : "#6DA2D9"};
+    background-color: ${props => props.clicked ? "#F8DEB9" : "#C1CCE0"};
     box-shadow: ${props => props.clicked ?
-            "0 0 0 1px #F0D264 inset, 0 0 0 2px rgba(255,255,255,0.15) inset, 0 8px 0 0 rgba(196, 172, 83, .7), 0 8px 0 1px rgba(0,0,0,.4), 0 8px 8px 1px rgba(0,0,0,0.5)" :
-            "0 0 0 1px #6698cb inset, 0 0 0 2px rgba(255,255,255,0.15) inset, 0 8px 0 0 rgba(110, 164, 219, .7), 0 8px 0 1px rgba(0,0,0,.4), 0 8px 8px 1px rgba(0,0,0,0.5)"
+            "0 0 0 1px #D4C59E inset, 0 0 0 2px rgba(255,255,255,0.15) inset, 0 8px 0 0 rgba(196, 172, 83, .7), 0 8px 0 1px rgba(0,0,0,.4), 0 8px 8px 1px rgba(0,0,0,0.5)" :
+            "0 0 0 1px #9AA4B8 inset, 0 0 0 2px rgba(255,255,255,0.15) inset, 0 8px 0 0 rgba(110, 164, 219, .7), 0 8px 0 1px rgba(0,0,0,.4), 0 8px 8px 1px rgba(0,0,0,0.5)"
             };
        
-    
+       
     cursor: pointer;
 
     &:hover {
-        background-color: ${props => props.clicked ? "#F0D264" : "#699DD1"};
+        background-color: ${props => props.clicked ? "#FBD995" : "#A4AFC5"};
     }
 
     &:active {
