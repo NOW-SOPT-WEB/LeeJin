c29957220c2e2a7bfd799e816b1c89d52d30dc67
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-04-29 11:34:50 +0900

    feat: Modal design 및 button 추가

--- CardGame/src/components/SuccessModal/index.jsx
+++ CardGame/src/components/SuccessModal/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import * as s from './styles'
+import bgSrc from '../../assets/modal.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
-
-const SuccessModal = React.forwardRef(() => {
+const SuccessModal = React.forwardRef(({handleModalClose}, ref) => {
   return (
-    <s.ModalContainer>
-        메롱
+    <s.ModalContainer src={bgSrc}>
+        <s.closeBtn>
+            <FontAwesomeIcon icon={faCircleXmark} size="lg" onClick={handleModalClose} cursor="pointer"/>
+        </s.closeBtn>
+        <s.homeBtn onClick={handleModalClose}>게임 또 하자</s.homeBtn>
     </s.ModalContainer>
   );
 });
--- CardGame/src/components/SuccessModal/styles.js
+++ CardGame/src/components/SuccessModal/styles.js
@@ -5,13 +5,39 @@ export const ModalContainer = styled.div`
     top: 50%;
     left: 50%;
 
-    width: 70%;
+    width: 50%;
     height: 50%;
 
-    padding: 2rem;
+    padding: 1rem;
 
     background-color: white;
-    box-shadow: "24px";
+    background-image: url(${(props) => props.src});
+    background-size: 100% 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    border-radius: 20px;
+    box-shadow: 24px;
 
     transform: translate(-50%, -50%);
 `
+
+export const closeBtn = styled.div`
+    display: flex;
+    justify-content: end;
+`;
+
+export const homeBtn = styled.div`
+    position: absolute;
+    top: 58%;
+    left: 50%;
+
+    padding: 0.8rem 2rem;
+
+    border-radius: 20px;
+
+    color: white;
+    background-color: black;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+`;
